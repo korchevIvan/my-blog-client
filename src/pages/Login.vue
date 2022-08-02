aa7b1aa6fdf87bf698dec25a6a10b4d48902d9c4
korchevIvan <unbsold@yandex.ru>
2022-08-02 23:52:19 +0300

    Authenticating

---
+++ src/pages/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <form v-on:submit.prevent="login">
+    <button type="submit">
+      Login
+    </button>
+  </form>
+</template>
+
+<script>
+import {useStore} from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    const login = () => {
+      store.dispatch('login', {
+        email: 'unbsold@yandex.ru',
+        password: '12345678'
+      })
+    }
+
+    return {
+      login
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from "../pages/Home.vue";
+import Login from "../pages/Login.vue";
 import Post from "../pages/Post.vue";
 
 const routes = [
@@ -8,6 +9,11 @@ const routes = [
         path: '/',
         name: 'home',
         component: Home
+    },
+    {
+        path: '/admin/login',
+        name: 'login',
+        component: Login
     },
     {
         path: '/posts/:slug',

f65ed267b31fbdf2ab4f5a5c54eecac9e79085f1
korchevIvan <unbsold@yandex.ru>
2022-08-08 20:45:27 +0300

    Textarea height component

--- src/pages/admin/Edit.vue
+++ src/pages/admin/Edit.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <textarea v-model="post.title"
-              class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0">
-    </textarea>
+    <ResizeTextarea v-if="post.title">
+      <template v-slot:default="{resize, el}">
+        <textarea :ref="el"
+            v-model="post.title"
+                  v-on:input="resize"
+                  class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0" row="1">
+      </textarea>
+      </template>
+    </ResizeTextarea>
   </div>
 </template>
 
@@ -10,8 +16,10 @@
 import useAdminPosts from "../../api/useAdminPosts";
 import {onMounted, watch} from "vue";
 import _ from 'lodash'
+import ResizeTextarea from "../../components/ResizeTextarea.vue";
 
 export default {
+  components: {ResizeTextarea},
   props: {
     slug: {
       required: true,

30dc25a2b07f3a40edd8e0dd9a52d7039aecd3d5
korchevIvan <unbsold@yandex.ru>
2022-08-08 21:02:18 +0300

    Designing the top edit bar

--- src/pages/admin/Edit.vue
+++ src/pages/admin/Edit.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <ResizeTextarea v-if="post.title">
-      <template v-slot:default="{resize, el}">
+    <div class="absolute w-full left-0 top-0 p-6 flex justify-between items-center space-x-6">
+      <div class="flex-grow flex items-center">
+        <span class="mr-1">/</span> <input type="text" class="p-0 border-none focus:ring-0 w-full" v-model="post.slug">
+      </div>
+      <div class="flex items-center space-x-6">
+        <div>
+          <span class="text-sm text-gray-500">Autosaved</span>
+        </div>
+        <button class="text-sm font-medium">
+          Published
+        </button>
+        <router-link :to="{name: 'post', params: {slug: post.slug}}" class="text-sm font-medium text-gray-800">
+          Preview
+        </router-link>
+
+      </div>
+    </div>
+    <div>
+      <ResizeTextarea v-if="post.title">
+        <template v-slot:default="{resize, el}">
         <textarea :ref="el"
-            v-model="post.title"
+                  v-model="post.title"
                   v-on:input="resize"
                   class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0" row="1">
       </textarea>
-      </template>
-    </ResizeTextarea>
+        </template>
+      </ResizeTextarea>
+    </div>
   </div>
 </template>
 
